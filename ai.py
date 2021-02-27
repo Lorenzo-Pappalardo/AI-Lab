f3a19fac8c9f765423dbc12ebac6c4df33c2300c
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-02-27 11:26:38 +0100

    Added more results

--- ai.py
+++ ai.py
@@ -10,7 +10,24 @@ def read_csv() -> DataFrame:
 
 def main() -> None:
     df = read_csv()
-    print(df.head)
+    total_rows = len(df)
+
+    missing_values = []
+    for row in df.columns:
+        null_rows = df[row].isnull().sum()
+        missing_values.append((null_rows/total_rows)*100)
+
+    cardinality = df.nunique()
+
+    max = df.max()
+    min = df.min()
+
+    first_quantile = df.quantile(.25)
+    third_quantile = df.quantile(.75)
+
+    mean = df.mean()
+    median = df.median()
+    standard_deviation = df.std()
 
 
 if (__name__ == '__main__'):
