58de1a983e68d13e7bf0aa082c8be5ff12aaaf2f
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-02-27 14:37:28 +0100

    Running time is now drastically reduced

--- ai.py
+++ ai.py
@@ -1,33 +1,44 @@
-import pandas
+import pandas as pd
 from pandas.core.frame import DataFrame
 
 
 def read_csv() -> DataFrame:
-    return pandas.read_csv('chicago_taxi_trips_2016_12.csv',
-                           parse_dates=True,
-                           infer_datetime_format=True)
+    return pd.read_csv('chicago_taxi_trips_2016_12.csv',
+                       parse_dates=True,
+                       infer_datetime_format=True)
 
 
 def main() -> None:
     df = read_csv()
-    total_rows = len(df)
 
-    missing_values = []
-    for row in df.columns:
-        null_rows = df[row].isnull().sum()
-        missing_values.append((null_rows/total_rows)*100)
+    numerics_df = df.select_dtypes(include='number')
+    categoricals_df = df.select_dtypes(exclude='number')
 
-    cardinality = df.nunique()
+    numerics_stats = pd.DataFrame(numerics_df.describe(include='all'))
+    categoricals_stats = pd.DataFrame(categoricals_df.describe(include='all'))
 
-    max = df.max()
-    min = df.min()
+    numerics = {
+        'total_number_of_values': len(numerics_df),
+        'percentage_of_missing_values': 100-(numerics_stats.loc['count']/len(numerics_df))*100,
+        'cardinality': numerics_df.nunique(),
+        'minimum_values': numerics_stats.loc['min'],
+        'maximum_values': numerics_stats.loc['max'],
+        'first_quartile': numerics_stats.loc['25%'],
+        'third_quartile': numerics_stats.loc['75%'],
+        'average': numerics_stats.loc['mean'],
+        'median': numerics_stats.loc['50%'],
+        'standard_deviation': numerics_stats.loc['std']
+    }
 
-    first_quantile = df.quantile(.25)
-    third_quantile = df.quantile(.75)
+    categoricals = {
+        'total_number_of_values': len(categoricals_df),
+        'percentage_of_missing_values': 100-(categoricals_stats.loc['count']/len(categoricals_df))*100,
+        'cardinality': categoricals_df.nunique(),
+        'mode': categoricals_df.mode()
+    }
 
-    mean = df.mean()
-    median = df.median()
-    standard_deviation = df.std()
+    """ print(numerics, end='\n\n')
+    print(categoricals, end='\n\n') """
 
 
 if (__name__ == '__main__'):
